976aa130f29dd90aed7be46b7ab71ca66887d147
Alex Laslău <george_laslau@yahoo.com>
2023-10-06 17:49:55 +0300

    Cleaned the comments

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -10,6 +10,8 @@ public:
         int j = 0;
         for (int i = 0; i < nums.size(); i++)
         {
+            //We check if item at [i] is not equal to val.
+            //If it is, we move it closer to the end of the array with the j++ index.
             if (nums[i] != val)
             {
                 nums[j++] = nums[i];
@@ -28,6 +30,11 @@ class Solution
 public:
     int removeElement(vector<int>& nums, int val)
     {
+        //This makes it cleaner. The Remove function actually just sends all the
+        //"val" items from the array at the end of the array and returns the index of the
+        //first "val" item in that array. So, we use then the erase function to
+        //delete all the "val" items. And we return k (which is the size of nums after
+        //the deletion).
         nums.erase(std::remove(nums.begin(), nums.end(), val), nums.end());
         return nums.size();
     }
